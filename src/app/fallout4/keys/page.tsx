a8d11816d35f0d3123df8d17f059fd66fb8ee886
vasha <vashavala@outlook.com>
2023-12-25 15:04:12 +0800

    pause the puppeteer

--- src/app/fallout4/keys/page.tsx
+++ src/app/fallout4/keys/page.tsx
@@ -1,42 +1,43 @@
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
 
-const crawler = async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  // default timeout 30 sec, set it to 5 min
-  page.setDefaultTimeout(60 * 5 * 1000);
+// const crawler = async () => {
+//   // Launch the browser and open a new blank page
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   // default timeout 30 sec, set it to 5 min
+//   page.setDefaultTimeout(60 * 5 * 1000);
 
-  // Navigate the page to a URL
-  console.log(`Navigate the page to a URL`)
-  const KEYS_URL = 'https://fallout.fandom.com/wiki/Fallout_4_keys'
-  const gotoRst = await page.goto(KEYS_URL);
+//   // Navigate the page to a URL
+//   console.log(`Navigate the page to a URL`)
+//   const KEYS_URL = 'https://fallout.fandom.com/wiki/Fallout_4_keys'
+//   const gotoRst = await page.goto(KEYS_URL);
 
-  const keyList = []
-  const tbodySelector = `.va-table tbody`
-  await page.waitForSelector(tbodySelector);
-  const tbodys = await page.$$(tbodySelector)
+//   const keyList = []
+//   const tbodySelector = `.va-table tbody`
+//   await page.waitForSelector(tbodySelector);
+//   const tbodys = await page.$$(tbodySelector)
 
-  if (!tbodys) return;
-  for (const tbody of tbodys) {
-    const trs = await tbody?.$$(`tr`);
-    if (!trs) return;
-    trs.shift();
-    for (const tr of trs) {
-      const nameCell = (await tr?.$(`td:nth-child(2) b`))
-      const name = await nameCell?.evaluate(el => el.textContent);
-      const idCell = (await tr?.$(`td:last-child span`))
-      const id = await idCell?.evaluate(el => el.textContent);
-      keyList.push({ id, name })
-    }
-    console.log(keyList.length)
-  }
-  console.log(`keyList`, keyList)
-  await browser.close();
-  return keyList
-}
+//   if (!tbodys) return;
+//   for (const tbody of tbodys) {
+//     const trs = await tbody?.$$(`tr`);
+//     if (!trs) return;
+//     trs.shift();
+//     for (const tr of trs) {
+//       const nameCell = (await tr?.$(`td:nth-child(2) b`))
+//       const name = await nameCell?.evaluate(el => el.textContent);
+//       const idCell = (await tr?.$(`td:last-child span`))
+//       const id = await idCell?.evaluate(el => el.textContent);
+//       keyList.push({ id, name })
+//     }
+//     console.log(keyList.length)
+//   }
+//   console.log(`keyList`, keyList)
+//   await browser.close();
+//   return keyList
+// }
 export default async function Page() {
-  const keyList = await crawler() || []
+  // pause the puppeteer
+  // const keyList = await crawler() || []
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello, KEYS Page!</h1>
